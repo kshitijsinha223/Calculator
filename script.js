a10600032c0cc51364e0d97d085627bafb8128a9
Kshitij <146977257+kshitijsinha223@users.noreply.github.com>
2024-03-12 18:17:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ arr.forEach(button=>{
             string=eval(string);
             input.value=string;
         }
-        else if(e.targer.innerHTML=='AC'){
+        else if(e.target.innerHTML=='AC'){
             string="";
             input.value=string;
         }
-        else if(e.targer.innerHTML=='DEL'){
+        else if(e.target.innerHTML=='DEL'){
             string=string.substring(0,string.length-1);
             input.value=string;
         }
